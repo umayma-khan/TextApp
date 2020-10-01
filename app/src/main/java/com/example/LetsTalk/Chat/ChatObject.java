acec13a3a55324ac7ca64fe43d920d3588b18d2b
umayma-khan <khanumayma99@gmail.com>
2020-10-01 12:34:55 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/LetsTalk/Chat/ChatObject.java
@@ -0,0 +1,30 @@
+package com.example.LetsTalk.Chat;
+
+import com.example.LetsTalk.Users.UserObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ChatObject implements Serializable {
+    private String chatId;
+
+    private ArrayList<UserObject> userObjectArrayList = new ArrayList<>();
+
+    public ChatObject(String chatId){
+        this.chatId = chatId;
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+    public ArrayList<UserObject> getUserObjectArrayList() {
+        return userObjectArrayList;
+    }
+
+
+
+
+    public void addUserToArrayList(UserObject mUser){
+        userObjectArrayList.add(mUser);
+    }
+}
---
+++ app/src/main/java/com/example/LetsTalk/Splash.java
@@ -0,0 +1,29 @@
+package com.example.LetsTalk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this, LoginActivity.class));
+                finish();
+                    }
+        },2000);
+
+    }
+}
